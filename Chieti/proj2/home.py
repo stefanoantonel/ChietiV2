3c0be70095b33ffbfefa1428442209024af8b1db
stefanoantonel <stefanoantonel@gmail.com>
2014-04-05 09:16:54 -0300

    merge

---
+++ Chieti/proj2/home.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.http import request, HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext, loader
+from django.template import Context, Template
+# Create your tests here.
+def index(request):
+
+    
+    fp = open('./proj2/templates/proj2/homePage.html')
+    t = Template(fp.read())
+    fp.close()
+    html = t.render(Context())
+    return HttpResponse(html)
+    
+    
