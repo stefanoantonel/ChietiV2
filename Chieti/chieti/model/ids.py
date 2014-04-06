4b02eee967a3beefbe9846ac64f95869521a98cb
stefanoantonel <stefanoantonel@gmail.com>
2014-04-06 13:14:18 -0300

    merge

---
+++ Chieti/chieti/model/ids.py
@@ -0,0 +1,9 @@
+
+#esta clase mepa que es la que le vamos a pedir que nos busque un id con diferentes metodos para cada clase,
+#osea que va a buscar el maximo de los id de la clase y nos devuelve ese+1 asi lo tenemos como id en la clase posta
+
+class ids(object):
+
+    def getItemId(self):
+        pass
+    
---
+++ Chieti/chieti/model/testMain.py
@@ -0,0 +1,8 @@
+#from chieti.models import product
+
+
+#from model.order import order
+if __name__ == '__main__':
+    
+    
+    pass
