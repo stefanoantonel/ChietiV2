3f01af6a31b69196c114a51cf517b5ab54e405a4
stefanoantonel <stefanoantonel@gmail.com>
2014-04-17 03:06:46 -0300

    con modulo de cambio de precio

--- Chieti/chieti/urls.py
+++ Chieti/chieti/urls.py
@@ -6,12 +6,16 @@ urlpatterns = patterns('',
     url(r'home/', 'chieti.views.home', name='home'),
     url(r'head/', 'chieti.mainHead.index', name='head'),
     url(r'addProduct/', 'chieti.views.addProd', name='addProd'),
-    url(r'hom/ad/', 'chieti.views.addProd2', name='addProd2'),
+    url(r'addProduct2/', 'chieti.views.addProd2', name='addProd2'),
     #url(r'^admin/', include(admin.site.urls)),
     #url('d/','chieti.tests.index',name='pro'),
     #url(r'$','chieti.tests.index'),
     #url('index/',include('chieti.urls')),
     url(r'product/','chieti.views.showProduct'),
+    url(r'changePrice/','chieti.views.changePrice'),
+    url(r'changePrice2/','chieti.views.changePrice2'),
+    url(r'addToOrder/','chieti.views.addToOrder'),
+
     
 )
 
