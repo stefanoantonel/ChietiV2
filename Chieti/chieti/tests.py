cfd7013b2bf8215dda0b2ea7fe61192f4a303f3c
stefanoantonel <stefanoantonel@gmail.com>
2014-05-14 00:41:47 -0300

    no imprime ordenes vacias ni entregadas

--- Chieti/chieti/tests.py
+++ Chieti/chieti/tests.py
@@ -1,11 +1,14 @@
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 
-from chieti.models import orderManager
+from chieti.models import orderManager, item, order
 
 
 # Create your tests here.
 if __name__ == '__main__':
+	orders=item.objects.all()
+	order1=order.objects.filter(getItem=orders)
 	
-	pass
+	print order1
+	
 	
