6208ff4cc82d639731eac2dae9e5c57def1aea2b
stefanoantonel <stefanoantonel@gmail.com>
2014-05-24 21:53:45 -0300

    m

--- Chieti/chieti/tests.py
+++ Chieti/chieti/tests.py
@@ -8,9 +8,6 @@ from chieti.models import order, product, item, orderManager, user, category
 
 # Create your tests here.
 if __name__ == '__main__':
+	p=product.objects.filter(id=1)
+	print p.query
 	
-	a='pedro'
-	cate=category.objects.all()
-	print cate
-	b={'c':a}
-	print b['c']
