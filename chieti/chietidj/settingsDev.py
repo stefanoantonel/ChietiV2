d6895627bc42562fe08a18e2eaf08eeb9f7fef99
stefanoantonel <stefanoantonel@gmail.com>
2014-06-27 16:35:25 -0300

    ..

--- chieti/chietidj/settingsDev.py
+++ chieti/chietidj/settingsDev.py
@@ -120,9 +120,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
 STATIC_URL = '/static/'
 #STATIC_DIR = '/home/webapps/static_media'
 #STATIC_ROOT = '/home/chietionline/webapps/static_media/'
