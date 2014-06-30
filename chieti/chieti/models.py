6ed34390ab85170f970e7b95f65f18838df55232
stefanoantonel <stefanoantonel@gmail.com>
2014-06-30 17:01:31 -0300

    __in summaryBuy

--- chieti/chieti/models.py
+++ chieti/chieti/models.py
@@ -48,7 +48,8 @@ class orderManager(models.Model):
 		orderNotDelivered = order.objects.filter(delivered='false',confirm='true')
 		it=item.objects.filter(orderFK__in=orderNotDelivered).values("productFK").annotate(quantity=models.Sum('quantity'))
 		prod=product.objects.filter(isPromo='true')
-		itP=it.filter(productFK=prod)
+		itP=it.filter(productFK__in=prod)
+		print(itP.query)
 
 		vector=[]
 		for i in it:
@@ -58,7 +59,8 @@ class orderManager(models.Model):
 			mu=product.objects.get(id=i["productFK"]).measureUnit
 			a={"product":prod,"quantity":quant, "measureUnit":mu}
 			vector.append(a)
-			
+		
+		#print(itP[0])	
 		for a in itP:
 			result=product.objects.get(id=a.get("productFK")).multItemPromo(a.get('quantity'))
 			for prodItem in result:
