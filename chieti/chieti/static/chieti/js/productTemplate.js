d9ae45019a9ee5adef1a0481fa199d3e43ed5e93
florbonansea <florenciabonanasea@gmail.com>
2014-07-06 22:25:20 -0300

    funciones
    saque algunas funciones del productTemplate.js porq tmb las uso en el
    home

--- chieti/chieti/static/chieti/js/productTemplate.js
+++ chieti/chieti/static/chieti/js/productTemplate.js
@@ -75,7 +75,9 @@ function popUpAutoComplete(id,nameProd,um,saleP,items) {
 		//console.log("se detuvo");
 	});
 
-	$('.add').click(addClick);
+	$('.add').click(function(){ addClick(this);
+	reloadTotalPrice();
+	});
 	$('#close').click(function(){
 		$(".divActual").remove();
 	});
@@ -150,83 +152,9 @@ $(document).ready(function() {
 	reloadTotalPrice();
 });
 
-function checkQuantOk(quant){
-	num=$(quant).val();
-	um=$(quant).siblings("#um").html();
-	if(!$.isNumeric(num)){
-		return false;
-	}
-	if(num < 0.1)
-		return false;
-	if (um=="Un"){
-		if(!(num % 1 === 0)){
-			return false
-		}
-	}
-	return true;
-}
-
-function addClick(){
-	console.log('çlick')
-	quant= $(this).siblings('input[name=quantity]');
-	if(!checkQuantOk(quant)){
-		  //chequear aveces no lo llama
-		alert("Cantidad incorrecta");
-		$(this).focus();
-		return;
-	}
-	var ids= $(this).siblings("input[name=ids]").attr("id");
-	
-	var quant= $(this).siblings('input[name=quantity]').val()
-	var quantAcheck= $(this).siblings('input[name=quantity]')
-	divActual=$(this);
-	if(checkQuantOk(quantAcheck)==true){
-		$.ajax({
-			url: '/chieti/addToOrder/',
-			type: 'get', 
-			data: {
-				'ids':ids,
-				'quantity':quant,
-				//'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val(),
-			},
-			success: function(data) {
-				
-				if (data != 'true'){
-					$('body').html(data);
-			/* var w  = 400;
-			var h = 300;
-			var left = (screen.width/2)-(w/2);
-			var top = (screen.height/2)-(h/2);
-			var myWindow = window.open('/chieti/singIn/?ventana=1','','toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);*/
-				}
-				else{
-					// Agregar efecto
-					$(divActual).parents(".divActual").remove();                	
-				}
-			}
 
 
-		});
-	}
-	else{
-		alert("Cantidad incorrecta");
-		$(this).focus();
-	}
-	reloadTotalPrice();
-}
 
-function setQuantity(){
-	$('.quantity').change(function(){
-		var ant=$(this).siblings(".price").attr('title');
-		var cant=$(this).val()            
-		var dsp=ant*cant
-		dsp = dsp.toFixed(2);
-		
-		
-		$(this).siblings('.price').html(dsp) 
-		 
-	});
-}
 
 function recargar(){
 	//tooltip
